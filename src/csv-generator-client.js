89b3d7a949aba9ee4853a33c18b1954608e3d111
Geovanny Fajardo <gfajardo@smartprocure.us>
2017-10-24 00:33:26 -0400

    testing

--- src/csv-generator-client.js
+++ src/csv-generator-client.js
@@ -1,6 +1,6 @@
 import _ from 'lodash/fp'
 
-export const getInstance = function ({ separator = ',', addQuotes = false }) {
+export const getInstance = function ({ separator = ',', addQuotes = false } = {}) {
   if (addQuotes) {
     separator = `"${separator}"`
   }
